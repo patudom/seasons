d0392f24645803d55d6d9049d7c8fb0f8ee739c6
Pat Udomprasert <pudompra@cfa.harvard.edu>
2025-10-15 17:18:16 -0400

    Undo test text

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -23,7 +23,7 @@
           tabindex="0"
           />
         <div id="splash-screen-text">
-          <p>FORK Test The Sun's Seasonal Journey</p>
+          <p>The Sun's Seasonal Journey</p>
         </div>
       </div>
 
